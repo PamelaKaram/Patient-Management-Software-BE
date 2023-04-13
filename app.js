ea2bafd39a27eb2646e9bab2e3630a11ee513da2
habibdeek <deek_habib@outlook.com>
2023-04-13 18:22:44 +0300

    Added API to fetch all appointments

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const connection = require('./database');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/user', (req,res)=>{
-    let sql= 'SELECT * FROM users';
+// api to get all the appointments and help the doctor check his availability
+app.get('/check',(req,res)=>{
+    let sql= 'SELECT appointments.*, users.first_name, users.last_name FROM appointments INNER JOIN users ON appointments.user_id = users.id';
     connection.query(sql, (err, result)=>{
         if(err) throw err;
         console.log(result);
+        res.json(result);
         res.send('Data fetched...');
     })
 })
+
+
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));

9dc29c4d34922eb692e6d1f514571cba6d80d3f3
habibdeek <deek_habib@outlook.com>
2023-04-13 19:54:22 +0300

    Added API to fetch all future appointments for patient

--- app.js
+++ app.js
@@ -55,6 +55,21 @@ app.post('/patient/checkPrevious',async (req, res)=>{
     const today = new Date();
     const dateString = today.toISOString().slice(0,10);
     const email = req.body.email;
+    let sql = await `SELECT appointments.*, users.email FROM users, appointments WHERE appointments.user_id=users.id AND users.email ='${email}'  AND date < '${dateString}'`;
+    connection.query(sql, (err, result)=>{
+        if(err) throw err;
+        console.log(result);
+        console.log(email);
+        res.json(result);
+        res.send('Data fetched...');
+    })
+})
+
+// api to get all the future appointments of a specific patient
+app.post('/patient/checkFuture',async (req, res)=>{
+    const today = new Date();
+    const dateString = today.toISOString().slice(0,10);
+    const email = req.body.email;
     let sql = await `SELECT appointments.*, users.email FROM users, appointments WHERE appointments.user_id=users.id AND users.email ='${email}'  AND date > '${dateString}'`;
     connection.query(sql, (err, result)=>{
         if(err) throw err;

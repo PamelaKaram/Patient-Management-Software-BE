8dbba04ad61c8026cede5ebe022eaf5f07c9a197
habibdeek <deek_habib@outlook.com>
2023-04-13 15:54:32 +0300

    First commit on branch

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const connection = require('./database');
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/user', (req,res)=>{
+    let sql= 'SELECT * FROM users';
+    connection.query(sql, (err, result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send('Data fetched...');
+    })
+})
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
--- database.js
+++ database.js
@@ -2,16 +2,16 @@ let mysql = require('mysql2');
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
     user: 'root',
-    password: 'user',
+    password: '',
     database: 'patient_software'
 });
 
-connection.connect(function(err) {
+connection.connect((err) =>{
     if (err) {
       return console.error('error: ' + err.message);
     }
-  
     console.log('Connected to the MySQL server.');
   });
+
+  module.exports= connection;

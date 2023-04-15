725b2eb5a685758a3975acc28a67e0ac217cee71
Jad <jadaboukaram2002@hotmail.com>
2023-04-15 15:08:25 +0200

    docker was not loading updated version of application, fixed

--- database.js
+++ database.js
@@ -1,17 +1,19 @@
 let mysql = require('mysql2');
 
-let connection = mysql.createConnection({
-    host: 'localhost',
+let db = mysql.createConnection({
+    host: 'mysql',
     port: 3306,
     user: 'root',
-    password: 'user',
+    password: 'password',
     database: 'patient_software'
 });
 
-connection.connect(function(err) {
+db.connect(function(err) {
     if (err) {
       return console.error('error: ' + err.message);
     }
   
     console.log('Connected to the MySQL server.');
   });
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const db = require('./database');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('vfergregregbebrbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbergre World!'));
+
+app.get('/test', (req, res) => res.send('Test!'));
+
+app.get("/createdb", (req, res) => {
+
+    let sql = "CREATE DATABASE nodemysql";
+  
+    db.query(sql, (err) => {
+  
+      if (err) {
+  
+        throw err;
+  
+      }
+  
+      res.send("Database created");
+  
+    });
+  
+  });
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));

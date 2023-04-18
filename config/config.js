93c3091d9c57c206f3599ab840818932d3441ef8
Jad <jadaboukaram2002@hotmail.com>
2023-04-18 19:36:40 +0200

    fixed migrations error, sequelize was not recognizing mysql host

--- config/config.js
+++ config/config.js
@@ -4,9 +4,26 @@ dotenv.config();
 export default {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD_DEV,
+    password: process.env.DB_PASSWORD_TYPESENSE,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST_DEV,
+    host: process.env.DB_HOST_TYPESENSE,
+    port: process.env.DB_PORT_TYPESENSE,
     dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD_TYPESENSE,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST_TYPESENSE,
+    port: process.env.DB_PORT_TYPESENSE,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD_TYPESENSE,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST_TYPESENSE,
+    dialect: "mysql",
+    port: process.env.DB_PORT_TYPESENSE,
   },
 };

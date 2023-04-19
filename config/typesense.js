885c16c131b899393719f1a64d5967dc6588a7c7
Jad <jadaboukaram2002@hotmail.com>
2023-04-19 12:27:45 +0200

    fixed typesense config, added autoUpdate to typesense index in routes

--- config/typesense.js
+++ config/typesense.js
@@ -1,19 +1,21 @@
 import Typesense from "typesense";
 import sequelize from "../src/models/index.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-let client = new Typesense.Client({
+export const client = new Typesense.Client({
   nodes: [
     {
-      host: "typesense", // For Typesense Cloud use xxx.a1.typesense.net
-      port: "8108", // For Typesense Cloud use 443
-      protocol: "http", // For Typesense Cloud use https
+      host: process.env.TYPESENSE_HOST, // For Typesense Cloud use xxx.a1.typesense.net
+      port: process.env.TYPESENSE_PORT, // For Typesense Cloud use 443
+      protocol: process.env.TYPESENSE_PROTOCOL, // For Typesense Cloud use https
     },
   ],
-  apiKey: "xyz",
+  apiKey: process.env.TYPESENSE_API_KEY,
   connectionTimeoutSeconds: 2,
 });
 
-let usersSchema = {
+export const usersSchema = {
   name: "users",
   fields: [
     { name: "firstName", type: "string" },
@@ -21,56 +23,45 @@ let usersSchema = {
   ],
 };
 
-try {
-  await client.collections("users").delete();
-  console.log("Deleting existing collection: users");
-} catch (error) {
-  // Do nothing
+export async function updateTypesense() {
+  try {
+    await client.collections("users").delete();
+    console.log("Deleting existing collection: users");
+  } catch (e) {
+    console.log("On deleting users: ", e);
+  }
+
+  client
+    .collections()
+    .create(usersSchema)
+    .then(function (data) {
+      console.log(data);
+    });
+
+  let usersData;
+  //convert to JSON to be able to import to typesense
+  try {
+    usersData = await sequelize.query(
+      `SELECT JSON_ARRAYAGG(JSON_OBJECT('_id', id, 'firstName', firstName, 'lastName', lastName))
+            FROM users;`
+    );
+    console.log(
+      usersData[0][0][
+        "JSON_ARRAYAGG(JSON_OBJECT('_id', id, 'firstName', firstName, 'lastName', lastName))"
+      ]
+    );
+  } catch (e) {
+    console.log(e);
+  }
+
+  client
+    .collections("users")
+    .documents()
+    .import(
+      usersData[0][0][
+        "JSON_ARRAYAGG(JSON_OBJECT('_id', id, 'firstName', firstName, 'lastName', lastName))"
+      ]
+    );
 }
 
-client
-  .collections()
-  .create(usersSchema)
-  .then(function (data) {
-    console.log(data);
-  });
-
-let usersData;
-//convert to JSON to be able to import to typesense
-try {
-  usersData = await sequelize.query(
-    `SELECT JSON_ARRAYAGG(JSON_OBJECT('_id', id, 'firstName', firstName, 'lastName', lastName, 'email', email))
-      FROM users;`
-  );
-  console.log(
-    usersData[0][0][
-      "JSON_ARRAYAGG(JSON_OBJECT('_id', id, 'firstName', firstName, 'lastName', lastName, 'email', email))"
-    ]
-  );
-} catch (e) {
-  console.log(e);
-}
-
-client
-  .collections("users")
-  .documents()
-  .import(
-    usersData[0][0][
-      "JSON_ARRAYAGG(JSON_OBJECT('_id', id, 'firstName', firstName, 'lastName', lastName, 'email', email))"
-    ]
-  );
-
-let searchParameters = {
-  q: "a",
-  query_by: "firstName",
-};
-
-client
-  .collections("users")
-  .documents()
-  .search(searchParameters)
-  .then(function (searchResults) {
-    console.log(searchResults);
-  });
-
-export default client;
+updateTypesense();
--- src/router.js
+++ src/router.js
@@ -5,60 +5,112 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import authenticated from "./middlewares/authentication.js";
 import isAuthorized from "./middlewares/authorization.js";
-import client from "../config/typesense.js";
+import { client, updateTypesense } from "../config/typesense.js";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const router = express.Router();
 
-router.post("/registerPatient", signupValidation, async (req, res) => {
-  // LOWER() for lower case
-  // escape() method sanitizes the input to prevent SQL injection
-  const {
-    firstName,
-    lastName,
-    email,
-    birthday,
-    phoneNumber,
-    createdAt,
-    updatedAt,
-  } = req.body;
-  console.log(req.body);
-  try {
-    const getUser = await sequelize.query(
-      `SELECT * FROM users WHERE LOWER(email) = LOWER(${sequelize.escape(
-        email
-      )});`
-    );
-    if (getUser[0].length > 0) {
-      return res.status(409).send({
-        msg: "Email already in use!",
+router.post(
+  "/registerPatient",
+  authenticated,
+  isAuthorized(ROLES.admin),
+  async (req, res) => {
+    // LOWER() for lower case
+    // escape() method sanitizes the input to prevent SQL injection
+    const {
+      firstName,
+      lastName,
+      email,
+      birthday,
+      phoneNumber,
+      createdAt,
+      updatedAt,
+    } = req.body;
+    try {
+      const getUser = await sequelize.query(
+        `SELECT * FROM users WHERE LOWER(email) = LOWER(${sequelize.escape(
+          email
+        )});`
+      );
+      if (getUser[0].length > 0) {
+        return res.status(409).send({
+          msg: "Email already in use!",
+        });
+      }
+      const password = generateRandomPassword(8);
+      const hash = await bcrypt.hash(password, 10);
+      await sequelize.query(
+        `INSERT INTO users (email, lastName, firstName, phoneNumber, birthday, password, role, createdAt, updatedAt) VALUES (${sequelize.escape(
+          email
+        )}, ${sequelize.escape(lastName)}, ${sequelize.escape(
+          firstName
+        )}, ${sequelize.escape(phoneNumber)}, ${sequelize.escape(
+          birthday
+        )}, ${sequelize.escape(password)}, ${sequelize.escape(
+          ROLES.PATIENT
+        )}, ${sequelize.escape(createdAt)}, ${sequelize.escape(updatedAt)});`
+      );
+      await updateTypesense();
+      return res.status(201).send({
+        msg: "The user has been registered",
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).send({
+        msg: e,
       });
     }
-    const password = generateRandomPassword(8);
-    const hash = await bcrypt.hash(password, 10);
-    await sequelize.query(
-      `INSERT INTO users (email, lastName, firstName, phoneNumber, birthday, password, role, createdAt, updatedAt) VALUES (${sequelize.escape(
-        email
-      )}, ${sequelize.escape(lastName)}, ${sequelize.escape(
-        firstName
-      )}, ${sequelize.escape(phoneNumber)}, ${sequelize.escape(
-        birthday
-      )}, ${sequelize.escape(password)}, ${sequelize.escape(
-        ROLES.PATIENT
-      )}, ${sequelize.escape(createdAt)}, ${sequelize.escape(updatedAt)});`
-    );
-    return res.status(201).send({
-      msg: "The user has been registered",
-    });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).send({
-      msg: e,
-    });
   }
-});
+);
+
+// pharmacy has firstName only
+router.post(
+  "/addPharmacy",
+  authenticated,
+  isAuthorized(ROLES.admin),
+  async (req, res) => {
+    // LOWER() for lower case
+    // escape() method sanitizes the input to prevent SQL injection
+    const { email, birthday, phoneNumber, createdAt, updatedAt, name } =
+      req.body;
+    try {
+      const getUser = await sequelize.query(
+        `SELECT * FROM users WHERE LOWER(email) = LOWER(${sequelize.escape(
+          email
+        )});`
+      );
+      if (getUser[0].length > 0) {
+        return res.status(409).send({
+          msg: "Email already in use!",
+        });
+      }
+      const password = generateRandomPassword(8);
+      const hash = await bcrypt.hash(password, 10);
+      await sequelize.query(
+        `INSERT INTO users (email, firstName, phoneNumber, birthday, password, role, createdAt, updatedAt) VALUES (${sequelize.escape(
+          email
+        )}, ${sequelize.escape(name)}, ${sequelize.escape(
+          phoneNumber
+        )}, ${sequelize.escape(birthday)}, ${sequelize.escape(
+          password
+        )}, ${sequelize.escape(ROLES.PHARMACY)}, ${sequelize.escape(
+          createdAt
+        )}, ${sequelize.escape(updatedAt)});`
+      );
+      await updateTypesense();
+      return res.status(201).send({
+        msg: "The pharmacy has been registered",
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).send({
+        msg: e,
+      });
+    }
+  }
+);
 
 const generateRandomPassword = (length) => {
   const chars =
@@ -150,21 +202,6 @@ router.get("/user", authenticated, async (req, res) => {
       msg: e,
     });
   }
-});
-
-router.get("/users", async (req, res) => {
-  let searchParameters = {
-    q: "j",
-    query_by: "firstName",
-  };
-
-  client
-    .collections("users")
-    .documents()
-    .search(searchParameters)
-    .then(function (searchResults) {
-      console.log(searchResults);
-    });
 });
 
 router.get("/admin", authenticated, isAuthorized(ROLES.admin), (req, res) => {

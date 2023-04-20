c349b911f8f9779d7dd92a756e75b2de54b9df78
Jad <jadaboukaram2002@hotmail.com>
2023-04-20 15:44:15 +0200

    create notifications model

---
+++ models/notifications.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class notifications extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  notifications.init({
+    type: DataTypes.STRING,
+    user: DataTypes.INTEGER,
+    time: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'notifications',
+  });
+  return notifications;
+};

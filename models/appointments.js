bb523b342290d45f35c0da87287bcf9f4fb4fc58
Jad <jadaboukaram2002@hotmail.com>
2023-04-20 16:15:58 +0200

    craeted appointment model

---
+++ models/appointments.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class appointments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  appointments.init({
+    patientId: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'appointments',
+  });
+  return appointments;
+};

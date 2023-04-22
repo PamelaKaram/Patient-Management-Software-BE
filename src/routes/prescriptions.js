7468d39da4a8b7e81f68adf563c39337f1f9c002
habibdeek <deek_habib@outlook.com>
2023-04-22 20:03:14 +0300

    Added API that retrieves prescriptions for pharmacy

---
+++ src/routes/prescriptions.js
@@ -0,0 +1,33 @@
+import express from "express";
+import sequelize from "../models/index.js";
+import authenticated from "../middlewares/authentication.js";
+import isAuthorized from "../middlewares/authorization.js";
+import Roles from "../enums/roles.js";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const router = express.Router();
+
+router.get(
+    "/getPrescription",
+    authenticated,
+    isAuthorized(Roles.PHARMACY),
+    async (req, res) => { 
+        const { email } = req.body;
+        try {
+            const prescription = await sequelize.query(
+                `'SELECT medical_prescriptions.*, users.email FROM users, medical_prescriptions WHERE medical_prescriptions.user_id = users.id AND users.email = ${sequelize.escape(email)}`
+            );
+            res.status(201).send({
+                msg: "Prescription fetched successfully!",
+                prescription,
+            });
+        } catch (err) {
+            res.status(500).send({
+                msg: "Error fetching prescription!",
+                err,
+            });
+        }
+    }
+);

3d6c94b38c8bae4be3ec002c4fe33b4e105c0971
Jad <jadaboukaram2002@hotmail.com>
2023-04-24 12:51:46 +0300

    add get next medicine hook, add to cron

--- src/cron.js
+++ src/cron.js
@@ -1,10 +1,11 @@
 import cron from "node-cron";
 import getNextAppointments from "./hooks/appointments.js";
+import getNextMedicine from "./hooks/medicines.js";
 
 export default function () {
   // runs every day at 6:00 am
   cron.schedule("0 6 * * *", async () => {
     await getNextAppointments();
-  });
+    await getNextMedicine();
   });
 }

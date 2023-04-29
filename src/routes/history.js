1aac62990ae3fca55beb9ac84f9c975a85f706fd
habibdeek <deek_habib@outlook.com>
2023-04-29 17:31:25 +0300

    Added Api that retrieves history

--- src/routes/history.js
+++ src/routes/history.js
@@ -9,7 +9,32 @@ dotenv.config();
 
 const router = express.Router();
 
-
+router.get(
+    "/getHistory",
+    authenticated,
+    isAuthorized([Roles.Patient, Roles.Doctor]),
+    async (req, res) => {
+        const { patientId } = req.query;
+        try {
+            const history = await sequelize.query(
+                `SELECT medical_histories.* 
+                FROM medical_histories, users 
+                WHERE medical_histories.patientId = users.uuid AND medical_histories.patientId = ${sequelize.escape(
+                    patientId
+                )};`
+            );
+            res.status(200).send({
+                msg: "History retrieved successfully!",
+                history,
+            });
+        } catch (err) {
+            res.status(500).send({
+                msg: "Error retrieving history!",
+                err: err.message,
+            });
+        }
+    }
+);
 
 
 export default router;

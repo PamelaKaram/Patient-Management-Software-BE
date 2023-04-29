c4b2229c1ea88fad4f8a446cc85a4d6862572207
habibdeek <deek_habib@outlook.com>
2023-04-29 09:07:09 +0300

    Added API that will allow user to ask questions

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -2,6 +2,9 @@ import express from "express";
 import sequelize from "../models/index.js";
 
 import * as dotenv from "dotenv";
+import authenticated from "../middlewares/authentication.js";
+import isAuthorized from "../middlewares/authorization.js";
+import Roles from "../enums/roles.js";
 dotenv.config();
 
 const router = express.Router();
@@ -18,6 +21,37 @@ router.post("/answer", async (req, res) => {
       message: "Answered successfully",
     });
   } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+router.post("/patientQuestion",
+  authenticated,
+  isAuthorized(Roles.PATIENT),
+  async (req, res)=>{
+  const { id, question} = req.body;
+  if(!question)
+  {
+    return res.status(400).json({msg: "Question is empty, please enter it"});
+  }
+  try {
+    await sequelize.query(
+      `INSERT INTO patient_questions (patientId, question, createdAt, updatedAt) VALUES (${sequelize.escape(
+        id
+      )}, ${sequelize.escape(question)},'${new Date()
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ")}', '${new Date()
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ")}')`
+    );
+    res.status(200).json({
+      message: "Question added successfully",
+    });
+  }catch(err){
     res.status(500).json({
       message: "Internal Server Error",
     });

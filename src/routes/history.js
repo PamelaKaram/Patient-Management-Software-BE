918924d24cb09e048125dde421a128f7c20a165f
habibdeek <deek_habib@outlook.com>
2023-04-29 16:33:59 +0300

    Created migration and files

---
+++ src/routes/history.js
@@ -0,0 +1,15 @@
+import express from "express";
+import sequelize from "../models/index.js";
+import authenticated from "../middlewares/authentication.js";
+import isAuthorized from "../middlewares/authorization.js";
+import Roles from "../enums/roles.js";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const router = express.Router();
+
+
+
+
+export default router;
